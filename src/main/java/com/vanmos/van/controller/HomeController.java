a46491a7a00f8d762dc0d13d37698d49f06c6f59
leonardomantovanello <rm95222@estudante.fieb.edu.br>
2025-10-10 11:10:41 -0300

    adição de endpoint

---
+++ src/main/java/com/vanmos/van/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.vanmos.van.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API Vanmos está funcionando!";
+    }
+}
